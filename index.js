93cc656c8ef700abe7fa437e3dacf0e1f02913b8
Kay Jay <kharris2k16@gmail.com>
2019-07-24 21:40:34 -0700

    mvp complete

--- index.js
+++ index.js
@@ -44,6 +44,20 @@ server.get("/api/users/:id", (req, res) => {
     .json({error: `The user information could not be retrieved ${err}`}))
 });
 
+//delete by ID
+server.delete("/api/users/:id", (req, res) => {
+  const { id } = req.params;
+  database
+    .remove(id)
+    .then(user =>
+      user
+        ? res.status(200).json(user)
+        : res.status(500).json({ error: `The user with the specified ID does not exist.` })
+        )
+    .catch(err => res.status(500)
+    .json({error: `The user information could not be retrieved ${err}`}))
+});
+
 //post request
 server.post('/api/users', (req, res) => {
   const { name, bio } = req.body;
@@ -67,6 +81,33 @@ else {
 }
 });
 
+//put request
+server.put("/api/users/:id", (req, res) => {
+  const { name, bio } = req.body;
+
+  if (!name || !bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
+  } else {
+    database
+      .update(req.params.id, req.body)
+      .then(user => {
+        if (user) {
+          res.status(200).json(user);
+        } else {
+          res.status(404).json({
+            message: "The user with the specified ID does not exist."
+          });
+        }
+      })
+      .catch(() => {
+        res.status(500).json({
+          errorMessage: "The user information could not be modified."
+        });
+      });
+  }
+});
 
 
 
